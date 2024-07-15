aba94c5eab9c3f63a94a3264c8b0d4a32036c537
Sumon009838 <suman@appscode.com>
2024-07-15 14:10:55 +0600

    formatted

--- cmd/start.go
+++ cmd/start.go
@@ -4,15 +4,10 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"BookServer/authentication"
 	"BookServer/data"
 	"BookServer/handler"
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/jwtauth/v5"
 	"github.com/spf13/cobra"
-	"log"
-	"net/http"
 )
 
 var (
@@ -21,16 +16,10 @@ var (
 	password string
 )
 
-// startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "This command starts the server",
+	Long:  `Through this cli command we can specify the port, username and password by using flags`,
 	Run: func(cmd *cobra.Command, args []string) {
 		address := "8000"
 		if len(port) > 0 {
@@ -48,36 +37,7 @@ to quickly create a Cobra application.`,
 			}
 		}
 		data.Init1()
-		r := chi.NewRouter()
-		r.Get("/ping", handler.Pong)
-		r.Post("/login", authentication.Login)
-		r.Get("/logout", authentication.Logout)
-		r.Group(func(r chi.Router) {
-			r.Route("/books", func(r chi.Router) {
-				r.Get("/", handler.Getbooks)
-				r.Get("/{id}", handler.Getbook)
-				r.Group(func(r chi.Router) {
-					r.Use(jwtauth.Verifier(data.TokenAuth))
-					r.Use(jwtauth.Authenticator(data.TokenAuth))
-					r.Post("/", handler.Addbook)
-					r.Put("/{id}", handler.Updatebook)
-					r.Delete("/{id}", handler.Deletebook)
-				})
-			})
-			r.Route("/authors", func(r chi.Router) {
-				r.Get("/", handler.Getauthors)
-				r.Get("/{id}", handler.Getauthor)
-			})
-			r.Route("/find", func(r chi.Router) {
-				r.Get("/{genre}", handler.Find_by_genre)
-			})
-		})
-		fmt.Println("Listening and Serving to ", address)
-		err := http.ListenAndServe("localhost:"+address, r)
-		if err != nil {
-			log.Fatalln(err)
-			return
-		}
+		handler.Allfunc(address)
 	},
 }
 
@@ -86,13 +46,4 @@ func init() {
 	startCmd.Flags().StringVarP(&port, "port", "n", "", "Server port")
 	startCmd.Flags().StringVarP(&username, "username", "u", "", "Username to login")
 	startCmd.Flags().StringVarP(&password, "password", "p", "", "Password to login")
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ handler/handlerfunc.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"BookServer/authentication"
+	"BookServer/data"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
+	"log"
+	"net/http"
+)
+
+func Allfunc(address string) {
+	r := chi.NewRouter()
+	r.Get("/ping", Pong)
+	r.Post("/login", authentication.Login)
+	r.Get("/logout", authentication.Logout)
+	r.Group(func(r chi.Router) {
+		r.Route("/books", func(r chi.Router) {
+			r.Get("/", Getbooks)
+			r.Get("/{id}", Getbook)
+			r.Group(func(r chi.Router) {
+				r.Use(jwtauth.Verifier(data.TokenAuth))
+				r.Use(jwtauth.Authenticator(data.TokenAuth))
+				r.Post("/", Addbook)
+				r.Put("/{id}", Updatebook)
+				r.Delete("/{id}", Deletebook)
+			})
+		})
+		r.Route("/authors", func(r chi.Router) {
+			r.Get("/", Getauthors)
+			r.Get("/{id}", Getauthor)
+		})
+		r.Route("/find", func(r chi.Router) {
+			r.Get("/{genre}", Find_by_genre)
+		})
+	})
+	fmt.Println("Listening and Serving to ", address)
+	err := http.ListenAndServe("localhost:"+address, r)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+}
